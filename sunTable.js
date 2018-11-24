759c8a53aa559dc4dcff8c9d778581f541e1e342
lewostrowski <43729519+lewostrowski@users.noreply.github.com>
2018-11-24 11:50:11 +0100

    Update sunTable.js
    *correct spell errors
    *deleted timeZone() to prevent data mulpiply
    *added dayLight()
    *move some variables due to scope and refresh

--- sunTable.js
+++ sunTable.js
@@ -7,35 +7,51 @@ let error = "Warning! Check API update time! If this text apear more then 1 per 
 
 function sunCycle() {
   $.getJSON(tdrLink, function(tdr) { // geting info
-    let sunRise = Date.parse(tdr.results.sunRise);
+    let sunRise = Date.parse(tdr.results.sunrise);
     let civilBegin = Date.parse(tdr.results.civil_twilight_begin);
-    let sunSet = Date.parse(tdr.results.sunSet);
+    let sunSet = Date.parse(tdr.results.sunset);
     let civilEnd = Date.parse(tdr.results.civil_twilight_end);
     console.log(error)
 
+     // convert UTC+0 to UTC+1, summer time included
+    let c = new Date();
+    let tz = c.getTimezoneOffset();
+    tz *= -60000
+    // update time values
+    sunRise += tz;
+    civilBegin += tz;
+    sunSet += tz;
+    civilEnd += tz;
+
     function sunPosition() { // on screen changes
-      function timeZone() { // convert UTC+0 to UTC+1, summer time included
-        let c = new Date();
-        let tz = c.getTimezoneOffset();
-        tz *= -60000
-        return tz
-      }
-      // update time values
-      sunRise += timeZone();
-      civilBegin += timeZone();
-      sunSet += timeZone();
-      civilEnd += timeZone();
+      let dateNow = Date.now(); // actual date in ms
       let dayDuration = civilEnd - dateNow;
 
-      if (dateNow >= civilBegin && dateNow < sunRise) {
-        console.log(dayDuration)
+        function dayLight() {
+        let dayDuration = civilEnd - dateNow;
+        let dayMinuteDuration = dayDuration / 60000
+        if (dayMinuteDuration >= 60) {
+          let remain = dayMinuteDuration / 60;
+          return remain
+        } else if (dayMinuteDuration < 60){
+          let remain = dayMinuteDuration;
+          return remain
+        } else {
+          return "Error"
+        }
+      }
+
+      if (dateNow >= civilBegin && dateNow < sunRise){
+        document.getElementById("dayDuration").innerHTML = "Pozostało " + dayLight() + " światła dziennego.";
         console.log("Civil twilight")
       } else if (dateNow >= sunRise && dateNow < sunSet) {
-        console.log(dayDuration)
+        document.getElementById("dayDuration").innerHTML = "Pozostało " + dayLight() + " światła dziennego.";
         console.log("Day")
       } else if (dateNow >= sunSet && dateNow < civilEnd) {
-        console.log("sunSet")
+        document.getElementById("dayDuration").innerHTML = "Trwa noc";
+        console.log("Sunset")
       } else {
+        document.getElementById("dayDuration").innerHTML = "Trwa noc";
         console.log("Night")
       }
     }
